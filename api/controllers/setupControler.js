215e2c6d2e25dbd203a881e0138cc715db2e2c5c
Phu169 <phu169@gmail.com>
2018-07-31 11:08:15 +0700

    Kết thúc bài 83

---
+++ api/controllers/setupControler.js
@@ -0,0 +1,15 @@
+var Todos = require("../models/todoModel");
+
+module.exports = function (app) {
+
+    var seedTodo = [
+        {
+            text: "Hocj Node.js",
+            isDone: "false"
+        },
+        {
+            text: "Hoc Angular.js",
+            isDone: false
+        }
+    ];
+}
